f5f5ec18fe44fd86c096fa9c31b6d7c301413adb
Charlie Roeder <croederdev@gmail.com>
2022-01-18 19:45:35 -0500

    created grid

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const grid = document.querySelector('.grid');
+
+
+function create16Divs(parent, className){
+    for (let i = 0; i < 16; i++){
+        const div = document.createElement('div');
+        div.classList.add(className);
+        parent.appendChild(div);
+    }
+}
+
+create16Divs(grid, 'column');
+
+
+const columns = document.querySelectorAll('.column');
+
+columns.forEach((column) => {
+    create16Divs(column, 'box')
+});
