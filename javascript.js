202716affeeda9fdadd5fd00b4b4a70ac7f11232
Charlie Roeder <croederdev@gmail.com>
2022-01-19 22:36:48 -0500

    organized code into functions, added button to change grid size

--- javascript.js
+++ javascript.js
@@ -1,33 +1,51 @@
 const grid = document.querySelector('.grid');
 
 
-function create16Divs(parent, className){
-    for (let i = 0; i < 16; i++){
+function createDivs(numOfDivs, parent, className){
+    for (let i = 0; i < numOfDivs; i++){
         const div = document.createElement('div');
         div.classList.add(className);
         parent.appendChild(div);
     }
 }
 
-create16Divs(grid, 'column');
+function createGrid(sideLength, parent){
+    createDivs(sideLength, parent, 'column');
+    const columns = document.querySelectorAll('.column');
+    columns.forEach((column) => {
+        createDivs(sideLength, column, 'box')
+    });
+}
 
-
-const columns = document.querySelectorAll('.column');
-
-columns.forEach((column) => {
-    create16Divs(column, 'box')
-});
-
-const boxes = document.querySelectorAll('.box');
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', () => {
-        box.setAttribute('style', 'background-color: black');
-    })
-})
+function addGridListener(){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.setAttribute('style', 'background-color: black');
+        })
+    });
+}
 
 const resetButton = document.querySelector(".reset");
 resetButton.addEventListener('click', () => {
     boxes.forEach((box) => {
         box.setAttribute('style', 'background-color: white');
     })
-})
+});
+
+const gridSizeButton = document.querySelector('.gridsize');
+gridSizeButton.addEventListener('click', () => {
+    const length = prompt('New grid size');
+    removeAllChildNodes(grid);
+    createGrid(length, grid);
+    addGridListener();
+});
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+createGrid(16,grid);
+addGridListener();
